9dc8e5bceeb08963685e29a88aab2883564707fe
hc3413 <horatiorjcox@gmail.com>
2024-05-28 20:22:40 +0100

    can now select devices and subset strings of devices and compare

--- VNAplot.py
+++ VNAplot.py
@@ -89,7 +89,7 @@ def import_data(data_path: str):
     return s2p_files
 
 
-def calibration(s2p_files, open, short, thru):
+def calibration(s2p_files, open, short, thru, OS_plot):
     # Function to generate an OpenShort De-embedding calibration and apply it to the on-wafer thru measurements
     # Inputs multiple open and short measurements and compares them to see which give the most effective de-embedding
     # Outputs a list of de-embedding data and plots of the thru measurements before and after de-embedding for each protocol
@@ -102,7 +102,6 @@ def calibration(s2p_files, open, short, thru):
     num_colors = len(open)*len(short)*len(thru)
     colors = plt.cm.jet(np.linspace(0,1,num_colors))
     color_count = 0
-    plt.figure(f'Open Short De-embedding')
     for count_o, o  in enumerate(open, start=0):
         for count_sh, sh in enumerate(short, start=0):
             cal = OpenShort(dummy_open=o.network, dummy_short=sh.network, name='OpenShort Calibration')
@@ -111,8 +110,10 @@ def calibration(s2p_files, open, short, thru):
             OS_th = [] #initiate list to store the de-embedded thru data for each open/short pair
             total_error = 0 #initialize variable to store the total error for each open/short pair
             for count_th,th in enumerate(thru, start=0):
-                th.network.plot_s_mag(m=1, n=0, color=colors[color_count], linestyle='dashed',label = f'Raw: {th.label}; {sh.label}; O{count_o}S{count_sh}T{count_th}')  # Plot only s21 with colorblind colormap
-                cal.deembed(th.network).plot_s_mag(m=1, n=0, color=colors[color_count], label = f'OS: {th.label}; {sh.label}; O{count_o}S{count_sh}T{count_th}')  # Plot only s21 with colorblind colormap
+                if OS_plot:
+                    plt.figure(f'Open Short De-embedding')
+                    th.network.plot_s_mag(m=1, n=0, color=colors[color_count], linestyle='dashed',label = f'Raw: {th.label}; {sh.label}; O{count_o}S{count_sh}T{count_th}')  # Plot only s21 with colorblind colormap
+                    cal.deembed(th.network).plot_s_mag(m=1, n=0, color=colors[color_count], label = f'OS: {th.label}; {sh.label}; O{count_o}S{count_sh}T{count_th}')  # Plot only s21 with colorblind colormap
                 #store the error for each de-embedding protocol in the respective array index
                 error_value = np.abs(np.sum(cal.deembed(th.network).s[:,1,0]))
                 total_error += error_value
@@ -127,39 +128,58 @@ def calibration(s2p_files, open, short, thru):
     print(f"Best de-embedding protocol: dm[{min_index}]={min(error)}, worst = {max(error)}")
     
     #Plot the best de-embedding protocol
-    plt.figure('Best De-embedding Protocol')
-    thru[0].network.plot_s_mag(m=1, n=0, color='red', linestyle='dashed',label = f'Raw')  # Plot only s21 with colorblind colormap
-    dm[min_index].deembed(thru[0].network).plot_s_mag(m=1, n=0, color='green', label = f'Best OS:dm[{min_index}]')  # Plot only s21 with colorblind colormap
-    dm[max_index].deembed(thru[0].network).plot_s_mag(m=1, n=0, color='blue', label = f'Worst OS:dm[{max_index}]')  # Plot only s21 with colorblind colormap
-    plt.show()
-    
+    if OS_plot:
+        plt.figure('Best De-embedding Protocol')
+        thru[0].network.plot_s_mag(m=1, n=0, color='red', linestyle='dashed',label = f'Raw')  # Plot only s21 with colorblind colormap
+        dm[min_index].deembed(thru[0].network).plot_s_mag(m=1, n=0, color='green', label = f'Best OS:dm[{min_index}]')  # Plot only s21 with colorblind colormap
+        dm[max_index].deembed(thru[0].network).plot_s_mag(m=1, n=0, color='blue', label = f'Worst OS:dm[{max_index}]')  # Plot only s21 with colorblind colormap
+   
     #only return the best de-embedding protocol
     return dm[min_index]
 
 
 
-# def calibration(s2p_files, open, short):
-# #on assumption of multiple measurements of cal_open and cal_short i will store them in dm_x and you can test the different calibrations effectiveness
-# dm = [] #initialize an empty list to store the de-embedded data
-# cal_thru_OS = [] #initialize an empty list to store the de-embedded data applied to the thru for reference to see how effective de-embedding is
-# for x in range(len(cal_open)):
-#     dm.append(OpenShort(dummy_open=cal_open[x].network, dummy_short=cal_short[x].network, name='OpenShort Calibration'))
-#     cal_thru_OS.append(dm[x].deembed(cal_thru[x].network))
-  
-#     num_colors = len(s2p_files)
-#     count = 0
-#     colors = plt.cm.jet(np.linspace(0,1,num_colors))
-#     for s in s2p_files:
-#         if s.dev_row == 1:
-#             print(s.dev_row, s.dev_col, s.state)
-#             plt.figure(f'Open Short De-embedding on Device_{s.dev_row}_{s.dev_col}, cal_num = {x}')
-#             s.network.plot_s_db(m=1, n=0, color=colors[count], linestyle='dashed',label = f'raw_{s.filename[-9:-4]}_{s.state}')  # Plot only s21 with red color and label it as raw_x_x
-#             dm[x].deembed(s.network).plot_s_db(m=1, n=0, color=colors[count], label = f'OS_{s.filename[-9:-4]}_{s.state}')  # Plot only s21 with colorblind colormap
-#         count += 1
+def keyplot(OS, dev, dev_selection_in, subset_in, x_limits, y_limits):
+    # Function to plot the data for the selected devices and states
+    # Needs dev_selection_in/subset_in to be local to function hence the _in suffix
     
-#     plt.figure(f'Open Short De-embedding on the on-wafer thru measurement, cal_num = {x}')
-#     cal_thru[x].network.plot_s_db(m=1, n=0, color='red', label = f'raw_{x}')  # Plot only s21 with red color and label it as raw_x_x
-#     cal_thru_OS[x].plot_s_db(m=1, n=0, color='green', label = f'OS_{x}')  # Plot only s21 with green color
+    # Check if the dev_selection_in is empty or 'all' and set the dev_selection_in to all devices if so
+    if not dev_selection_in or dev_selection_in == 'all':
+        dev_selection_in = dev.keys()
+
+    # If subset_in is empty or 'all' then set the subset_check to always return True 
+    if not subset_in or subset_in == 'all':
+        subset_check = lambda x: True
+    # Else check if the filename contains any of the subset_in strings returning true/false to plot/not plot
+    else:
+        subset_check = lambda x: any(m in x.filename.lower() for m in subset_in)
+   
+    # Loop over the selected devices    
+    for key in dev_selection_in:
+        if key in dev: #extract the s2p files for the selected devices
+            value = dev[key] 
+            
+            fig, ax = plt.subplots()
+            ax.set_title(f'Device_{key}')
+            ax.set_xlabel('Frequency (Hz)')
+            ax.set_ylabel('Magnitude (dB)')
+            
+            #ax.set_xlim(x_limits)
+            #ax.set_ylim(y_limits)
+            num_colors = sum(1 for r in value if subset_check(r)) #count the number of files that match the subset criteria to set the number of colors            
+            colors = plt.cm.jet(np.linspace(0,1,num_colors))
+            color_count = 0
+            
+            # Loop over the s2p files for each select device plotting all that match the subset criteria
+            for r in value:
+                if subset_check(r):
+                    #ax.plot(r.network.s_mag[:, 1, 0], color=colors[color_count], linestyle='dashed',label = f'Raw: {r.label}')  # Plot only s21 with colorblind colormap
+                    data = OS.deembed(r.network)
+                    ax.plot(np.log10(data.f), data.s_db[:, 1, 0], color=colors[color_count], label = f'OS: {r.filename}')  # Plot only s21 with colorblind colormap          
+                    color_count += 1
+                
+            ax.legend()
+    return fig, ax
 
 
 
@@ -169,13 +189,13 @@ def calibration(s2p_files, open, short, thru):
 # Define the path to the directory containing the VNA data
 #directory = ('/Users/horatiocox/Desktop/VNA_Analysis/mag_angle_260424/')
 
-#directory = ('/Users/horatiocox/Desktop/VNA_Analysis/CPW_mem_oscillator_220524/Memristor/')
-directory = ('/Users/horatiocox/Desktop/VNA_Analysis/CPW_mem_oscillator_220524/Oscillator/')
+directory = ('/Users/horatiocox/Desktop/VNA_Analysis/CPW_mem_oscillator_220524/Memristor/')
+#directory = ('/Users/horatiocox/Desktop/VNA_Analysis/CPW_mem_oscillator_220524/Oscillator/')
 
 # Import the data from the VNA files
 s2p_files = import_data(directory)
-#for f in s2p_files:
-    #print(f.filename,f.run, f.label)
+for f in s2p_files:
+    print(f.filename,f.run, f.label)
 
 
 #-------------------Grouping-------------------
@@ -203,24 +223,19 @@ for s in s2p_files:
 
 #-------------------De-Embedding-------------------
 print('open_short_thru',len(cal_open),len(cal_short),len(cal_thru))
-
-OS = calibration(s2p_files, cal_open, cal_short, cal_thru) #calibration object outputted from all the on wafer measurements
+OS_plot = False #whether to plot the de-embedding results
+OS = calibration(s2p_files, cal_open, cal_short, cal_thru,OS_plot) #calibration object outputted from all the on wafer measurements
 
 #-------------------Plotting-------------------
 
-for key, value in dev.items():
-    plt.figure(f'Device_{key}')
-    color_count = 0
-    print('key',key)
-    num_colors = len(value)
-    colors = plt.cm.jet(np.linspace(0,1,num_colors))
-    for r in value:
-        print('color_count',color_count)
-        print('r',r.filename)
-        #r.network.plot_s_mag(m=1, n=0, color=colors[color_count], linestyle='dashed',label = f'Raw: {r.label}')  # Plot only s21 with colorblind colormap
-        OS.deembed(r.network).plot_s_mag(m=1, n=0, color=colors[color_count], label = f'OS: {r.filename}')  # Plot only s21 with colorblind colormap          
-        color_count += 1  
-        
+dev_selection = ['r2c1'] #specific devices to plot (all/empty plots all devices)
+subset = ['formed']; #Select only runs that contain any of the subset strings in their (all/empty plots all states)
+x_limits = [0, 20e9] #limits for the x-axis of the plot
+y_limits = [0, 1] #limits for the y-axis of the plot
+#keyplot(OS, dev, dev_selection,subset, x_limits, y_limits)
+
+fig_pristine, ax_pristine = keyplot(OS, dev, dev_selection,['pristine'], x_limits, y_limits)
+fig_formed, ax_formed = keyplot(OS, dev, dev_selection,['formed'], x_limits, y_limits)
 plt.show()
 
 
